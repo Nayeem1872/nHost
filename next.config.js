3aff6c4899e828da858d0073df4bfeee9d908c0b
nayeem1872 <nayeem.islam.1872@gmail.com>
2024-07-13 16:36:39 +0600

    new

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
       return [
         {
           source: "/api/:path*",
-          destination: "https://latino-wax-unfortunately-recognised.trycloudflare.com/api/:path*",
+          destination: "https://idaho-william-november-forest.trycloudflare.com/api/:path*",
         
         },
       ];
